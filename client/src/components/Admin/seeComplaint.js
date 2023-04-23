191132fd6d8e9ee474f0995bbd481dec6479b35e
Raghav <2020csb1115@iitrpr.ac.in>
2023-04-24 00:35:50 +0530

    filters

--- client/src/components/Admin/seeComplaint.js
+++ client/src/components/Admin/seeComplaint.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ComplaintCard from "./ComplaintCard";
 
-
 const SeeComplaint = () => {
-
-
     const [complaints, setComplaints] = useState([]);
+    const [filter, setFilter] = useState("all");
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
-        axios.get("/admin/getcomplaints")
+        axios
+            .get("/admin/getcomplaints")
             .then((response) => {
-                console.log(response.data);
                 setComplaints(response.data);
             })
             .catch((error) => {
@@ -20,24 +18,61 @@ const SeeComplaint = () => {
             });
     }, []);
 
+    const handleFilterClick = (filterStatus) => {
+        setFilter(filterStatus);
+    };
 
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
     return (
-
         <>
             <div>
-                {complaints.map((complaint) => (
-                    <ComplaintCard
-                        id={complaint.complaint_id}
-                        name={complaint.name}
-                        description={complaint.complaint_details}
-                        date={complaint.complaint_date}
-                    />
-                ))}
+                <div class="flex overflow-hidden bg-white border divide-x rounded-lg rtl:flex-row-reverse dark:bg-gray-900 dark:border-gray-700 dark:divide-gray-700">
+                    <button onClick={() => handleFilterClick('all')} class="px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+                        All
+                    </button>
+
+                    <button onClick={() => handleFilterClick('solved')} class="px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+                        Solved
+                    </button>
+
+                    <button onClick={() => handleFilterClick('unsolved')} class="px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 sm:text-base sm:px-6 dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
+                        Unsolved
+                    </button>
+                </div>
+                <br></br>
+                <div>
+                    <label for="username" class="block text-sm text-gray-500 dark:text-gray-300">Search By Name :</label>
+
+                    <input type="text" placeholder="Search by name" onChange={handleSearch} class="block  mt-2 placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+                </div>
+                {/* <div>
+                    <input type="text" placeholder="Search by name" onChange={handleSearch} />
+                </div> */}
+                <div>
+                    {complaints
+                        .filter((complaint) => {
+                            if (filter === "all" || (filter === "solved" && complaint.complaint_status === "done") || (filter === "unsolved" && complaint.complaint_status !== "done")) {
+                                return complaint.name.toLowerCase().includes(searchTerm.toLowerCase());
+                            } else {
+                                return false;
+                            }
+                        })
+                        .map((complaint) => (
+                            <ComplaintCard
+                                key={complaint.complaint_id}
+                                id={complaint.complaint_id}
+                                name={complaint.name}
+                                description={complaint.complaint_details}
+                                date={complaint.complaint_date}
+                            />
+                        ))}
+                </div>
             </div>
         </>
     );
-
 };
 
 export default SeeComplaint;
