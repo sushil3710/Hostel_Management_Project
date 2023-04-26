2ec07ea5363f6e7b4786e31641ed9bc8fa6b486d
Rohan Khanna <rohankhanna190@gmail.com>
2023-04-26 10:41:19 +0530

    db correction

--- server/database.sql
+++ server/database.sql
@@ -1,80 +1,32 @@
-  -- Active: 1680711743255@@127.0.0.1@5432@mtech
-  \c postgres
-  drop database hostel;
-  create database hostel;
-  \c hostel
-  CREATE TABLE signup_verification (
-      email_id TEXT PRIMARY KEY,
-      hashed_otp TEXT,
-      expiration_time TIMESTAMP
-  );
+-- Active: 1680711743255@@127.0.0.1@5432@mtech
+CREATE TABLE signup_verification (
+    email_id TEXT PRIMARY KEY,
+    hashed_otp TEXT,
+    expiration_time TIMESTAMP
+);
 
-  CREATE TABLE forgot_password_verification (
-      email_id TEXT PRIMARY KEY,
-      hashed_otp TEXT,
-      expiration_time TIMESTAMP
-  );
+CREATE TABLE forgot_password_verification (
+    email_id TEXT PRIMARY KEY,
+    hashed_otp TEXT,
+    expiration_time TIMESTAMP
+);
 
+CREATE TABLE hostel_details (
+    email_id TEXT PRIMARY KEY,
+    hostel_name TEXT,
+    hostel_wing TEXT,
+    hostel_floor TEXT,
+    hostel_room TEXT,
+);
 
-  CREATE TABLE room_change_request(
-    id SERIAL PRIMARY KEY,
-    email_id TEXT,
-    prev_room TEXT,
-    req_room TEXT,
-    reason TEXT,
-    comments TEXT,
-    isexchange TEXT,
-    phone TEXT,
-    exchange_id TEXT,
-    request_status TEXT DEFAULT 'pending',
-    admin_comment TEXT DEFAULT 'na',
-    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE login_verification (
+    email_id TEXT PRIMARY KEY,
+    hashed_otp TEXT,
+    expiration_time TIMESTAMP
+);
 
-  );
-
-  CREATE TABLE login_verification (
-      email_id TEXT PRIMARY KEY,
-      hashed_otp TEXT,
-      expiration_time TIMESTAMP
-  );
-
-  CREATE TABLE student_info (
-      -- Primary Keys
-      email_id TEXT PRIMARY KEY,
-      passwd TEXT,
-      hostel_id SERIAL,
-
-      -- Personal Details
-      full_name TEXT,
-      guardian TEXT,
-      fathers_name TEXT,
-      profile_image_url TEXT,
-      date_of_birth TEXT,
-      aadhar_card_number TEXT,
-      category TEXT,
-      is_pwd TEXT,
-      pwd_type TEXT,
-      blood_group TEXT,
-      nationality TEXT,
-      gender TEXT,
-      room TEXT,
-      -- Communication Details
-      communication_address TEXT,
-      communication_city TEXT,
-      communication_state TEXT,
-      communication_pincode TEXT,
-
-      permanent_address TEXT,
-      permanent_city TEXT,
-      permanent_state TEXT,
-      permanent_pincode TEXT,
-
-      mobile_number TEXT,
-      alternate_mobile_number TEXT
-  );
-
-  CREATE TABLE admins(
-    name TEXT, 
+CREATE TABLE student_info (
+    -- Primary Keys
     email_id TEXT PRIMARY KEY,
     passwd TEXT,
     entry_numb TEXT,
@@ -117,6 +69,23 @@ CREATE TABLE admins(
   admin_type INT NOT NULL
   
 );
+
+CREATE TABLE room_change_request(
+    id SERIAL PRIMARY KEY,
+    email_id TEXT,
+    prev_room TEXT,
+    req_room TEXT,
+    reason TEXT,
+    comments TEXT,
+    isexchange TEXT,
+    phone TEXT,
+    exchange_id TEXT,
+    request_status TEXT DEFAULT 'pending',
+    admin_comment TEXT DEFAULT 'na',
+    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+  );
+
 CREATE TABLE excels(
   name TEXT, 
   file_url TEXT,
@@ -160,24 +129,24 @@ CREATE TABLE complaint_details (
 
 
 
-  -- 0 for super-admin, 1 for faculty-admins, 2 for applicant, 3 for Staff
+-- 0 for super-admin, 1 for faculty-admins, 2 for applicant, 3 for Staff
 
-  CREATE OR REPLACE FUNCTION insert_into_login_verification()
-    RETURNS TRIGGER 
-    LANGUAGE PLPGSQL
-    AS
-  $$
-  BEGIN
-      INSERT INTO login_verification(email_id) VALUES(new.email_id);
-    RETURN NEW;
-  END;
-  $$;
+CREATE OR REPLACE FUNCTION insert_into_login_verification()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+    INSERT INTO login_verification(email_id) VALUES(new.email_id);
+	RETURN NEW;
+END;
+$$;
 
-  CREATE TRIGGER trigger_insert_into_admins
-    AFTER INSERT
-    ON admins
-    FOR EACH ROW
-    EXECUTE PROCEDURE insert_into_login_verification();
+CREATE TRIGGER trigger_insert_into_admins
+  AFTER INSERT
+  ON admins
+  FOR EACH ROW
+  EXECUTE PROCEDURE insert_into_login_verification();
 
 CREATE TRIGGER trigger_insert_into_student_info
   AFTER INSERT
