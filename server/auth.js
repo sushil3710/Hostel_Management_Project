9b841fa0a538696d60d2abfdfcc978793818ddb7
Rohit Kinha <2020csb1118@iitrpr.ac.in>
2023-04-27 02:28:20 +0530

    First Test

--- server/auth.js
+++ server/auth.js
@@ -28,64 +28,6 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-/**
- * Auth role tokens:
- * 0 for super-admin
- * 1 for faculty-admins/supervisors
- * 2 for student_info
- * 3 for staff from academics
- */
-
-const signin_otp = async (req, res) => {
-  email = req.body.email;
-
-  if (email === "") return res.send("0");
-
-  const result = await pool.query(
-    "select * from login_verification where email_id = $1",
-    [email]
-  );
-
-  if (result.rowCount === 0) return res.send("1");
-
-  const filePath = path.join(__dirname, "otp_email.html");
-  const html = fs.readFileSync(filePath, "utf-8").toString();
-  var template = handlebars.compile(html);
-
-  otp = otpGenerator.generate(6, { specialChars: false });
-
-  var replacements = {
-    VERIFICATION_CODE: otp,
-  };
-  var htmlToSend = template(replacements);
-
-  var mailOptions = {
-    from: "IIT Ropar",
-    to: "email_id_to_send_otp",
-    subject: "OTP for Sign-in",
-    html: htmlToSend,
-  };
-
-  mailOptions.to = email;
-  console.log(otp);
-
-  /** encrypt otp and save in db */
-  await bcrypt.hash(otp, saltRounds, async function (err, hash) {
-    await pool.query(
-      "UPDATE login_verification SET hashed_otp = $1, expiration_time = to_timestamp($2) WHERE email_id = $3",
-      [hash, Date.now() / 1000.0 + 600, email]
-    );
-  });
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    }
-  });
-
-  return res.send("2");
-};
-
 const signin_verify = async (req, res) => {
 
   const { email, password } = req.body;
@@ -106,29 +48,23 @@ const signin_verify = async (req, res) => {
 
   let userData;
   if (applicantRow) {
-   
-    //bcrypt.compare(password, applicantRow.passwd, (err, result) => 
-    const match = await bcrypt.compare(password,applicantRow.passwd );
-    if(match || applicantRow.passwd==="root"){
-        userData = {
-          userEmail: email,
-          userRole: 2,
-          department: null,
-        };
-        const jwtSecretKey = process.env.JWT_SECRET_KEY;
-        const authToken = jwt.sign(userData, jwtSecretKey);
-        return res.send({ result: 1, token: authToken });
- 
+    const match = await bcrypt.compare(password, applicantRow.passwd);
+    if (match || applicantRow.passwd === "root") {
+      userData = {
+        userEmail: email,
+        userRole: 2,
+        department: null,
+      };
+      const jwtSecretKey = process.env.JWT_SECRET_KEY;
+      const authToken = jwt.sign(userData, jwtSecretKey);
+      return res.send({ result: 1, token: authToken });
     }
     else {
       return res.send({ result: 0 });
     }
-   
   }
-  
+  if (adminRow) {
 
-   if (adminRow) {
-  
     userData = {
       userEmail: email,
       userRole: adminRow.admin_type,
@@ -136,35 +72,35 @@ const signin_verify = async (req, res) => {
     };
     var match;
 
-    if(email==='2020csb1132@iitrpr.ac.in' || email==='2020csb1118@iitrpr.ac.in' || email==='2020csb1109@iitrpr.ac.in'  ){
-       if(password===adminRow.passwd){
-        match=true;
-       }
-       else{
-        match=false;
-       }
+    if (email === '2020csb1132@iitrpr.ac.in' || email === '2020csb1118@iitrpr.ac.in' || email === '2020csb1109@iitrpr.ac.in') {
+      if (password === adminRow.passwd) {
+        match = true;
+      }
+      else {
+        match = false;
+      }
     }
-    else{
-      match = await bcrypt.compare(password,adminRow.passwd);
+    else {
+      match = await bcrypt.compare(password, adminRow.passwd);
     }
-    
-    if(match || adminRow.passwd==="root"){
-    
-        const jwtSecretKey = process.env.JWT_SECRET_KEY;
-        const authToken = jwt.sign(userData, jwtSecretKey);
 
-        switch (adminRow.admin_type) {
-          case 0:
-            return res.send({ result: 3, token: authToken, admin_type: 0 });
-          case 1:
-            return res.send({ result: 4, token: authToken, admin_type: 1 });
-          case 3:
-            return res.send({ result: 5, token: authToken, admin_type: 3 });
-          default:
-            return res.send({ result: 0 });
-        }
-   
-    }   else {
+    if (match || adminRow.passwd === "root") {
+
+      const jwtSecretKey = process.env.JWT_SECRET_KEY;
+      const authToken = jwt.sign(userData, jwtSecretKey);
+
+      switch (adminRow.admin_type) {
+        case 0:
+          return res.send({ result: 3, token: authToken, admin_type: 0 });
+        case 1:
+          return res.send({ result: 4, token: authToken, admin_type: 1 });
+        case 3:
+          return res.send({ result: 5, token: authToken, admin_type: 3 });
+        default:
+          return res.send({ result: 0 });
+      }
+
+    } else {
       return res.send({ result: 0 });
     }
   }
@@ -172,118 +108,7 @@ const signin_verify = async (req, res) => {
   return res.send({ result: 0 });
 };
 
-
-const signup_otp = async (req, res) => {
-  email = req.body.email;
-
-  if (email === "") return res.send("0");
-
-  const result = await pool.query(
-    "select * from login_verification where email_id = $1",
-    [email]
-  );
-
-  if (result.rowCount === 1) return res.send("1");
-
-  const filePath = path.join(__dirname, "otp_email.html");
-  const html = fs.readFileSync(filePath, "utf-8").toString();
-  var template = handlebars.compile(html);
-
-  otp = otpGenerator.generate(6, { specialChars: false });
-
-  var replacements = {
-    VERIFICATION_CODE: otp,
-  };
-  var htmlToSend = template(replacements);
-
-  var mailOptions = {
-    from: "IIT Ropar",
-    to: "email_id_to_send_otp",
-    subject: "OTP for Sign-up",
-    html: htmlToSend,
-  };
-
-  mailOptions.to = email;
-  console.log(otp);
-
-  const ifexists = await pool.query(
-    "select * from signup_verification where email_id = $1",
-    [email]
-  );
-
-  /** encrypt otp and save in db */
-  if (ifexists.rowCount === 0) {
-    /** First time sign-up */
-    await bcrypt.hash(otp, saltRounds, async function (err, hash) {
-      await pool.query(
-        "INSERT INTO signup_verification(email_id, hashed_otp, expiration_time) VALUES($1, $2, to_timestamp($3))",
-        [email, hash, Date.now() / 1000.0 + 600]
-      );
-    });
-  } else {
-    /** If there is already an entry (helpful for resend OTP feature) */
-    await bcrypt.hash(otp, saltRounds, async function (err, hash) {
-      await pool.query(
-        "UPDATE signup_verification SET hashed_otp = $1, expiration_time = to_timestamp($2) WHERE email_id = $3",
-        [hash, Date.now() / 1000.0 + 600, email]
-      );
-    });
-  }
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    }
-  });
-
-  return res.send("2");
-};
-
-const signup_verify = async (req, res) => {
-  const { email, otp,password,confirm_password } = req.body;
-
-
-  if (otp === "") return res.send({ result: 3 });
-
-  /** encrypt and check for otp in db and return accordingly */
-  const result = await pool.query(
-    "select * from signup_verification where email_id = $1",
-    [email]
-  );
-  const result_row = result.rows[0];
-
-  /** check if otp is expired */
-  if (Date.now() > new Date(result_row.expiration_time.getTime())) {
-    return res.send({ result: 2 });
-  }
-  if (password!==confirm_password) {
-    return res.send({ result: 4 });
-  }
-
-  const match = await bcrypt.compare(otp, result_row.hashed_otp);
-  if (match) {
-    await bcrypt.hash(password, saltRounds, async function (err, hash) {
-      await pool.query("insert into student_info(email_id, passwd) values($1, $2)", [
-        email, hash
-      ]);
-    });
-
-    const jwtSecretKey = process.env.JWT_SECRET_KEY;
-    const data = {
-      userEmail: email,
-      userRole: 2,
-      department: null,
-    };
-    const authToken = jwt.sign(data, jwtSecretKey);
-    return res.send({ result: 1, token: authToken });
-  } else {
-    return res.send({ result: 0 });
-  }
-
-};
-
-
-const forgot_password_otp = async (req, res) => { 
+const forgot_password_otp = async (req, res) => {
 
   email = req.body.email;
 
@@ -299,12 +124,12 @@ const forgot_password_otp = async (req, res) => {
   );
 
   //no email registered
- if (result1.rowCount === 0 && result2.rowCount === 0) return res.send("1");
+  if (result1.rowCount === 0 && result2.rowCount === 0) return res.send("1");
 
   const filePath = path.join(__dirname, "otp_email.html");
   const html = fs.readFileSync(filePath, "utf-8").toString();
   var template = handlebars.compile(html);
- 
+
 
   otp = otpGenerator.generate(6, { specialChars: false });
 
@@ -360,7 +185,7 @@ const forgot_password_otp = async (req, res) => {
 
 
 const forgot_password_verify = async (req, res) => {
-  const { email, otp,password,confirm_password } = req.body;
+  const { email, otp, password, confirm_password } = req.body;
 
 
   if (otp === "") return res.send({ result: 0 });
@@ -376,14 +201,14 @@ const forgot_password_verify = async (req, res) => {
     "select * from student_info where email_id = $1",
     [email]
   );
-const applicant_row=result1.rows[0];
+  const applicant_row = result1.rows[0];
 
 
   const result2 = await pool.query(
     "select * from admins where email_id = $1",
     [email]
   );
-  const admin_row=result2.rows[0];
+  const admin_row = result2.rows[0];
 
 
   /** check if otp is expired */
@@ -391,7 +216,7 @@ const applicant_row=result1.rows[0];
     return res.send({ result: 2 });
   }
 
-  if (password!==confirm_password) {
+  if (password !== confirm_password) {
     return res.send({ result: 3 });
   }
 
@@ -399,61 +224,55 @@ const applicant_row=result1.rows[0];
 
   const match = await bcrypt.compare(otp, result_row.hashed_otp);
   if (match) {
-    if(applicant_row){
+    if (applicant_row) {
       await bcrypt.hash(password, saltRounds, async function (err, hash) {
         await pool.query("update student_info set passwd=$1 where email_id=$2", [
-          hash,email
-       ]);
+          hash, email
+        ]);
       });
-    const jwtSecretKey = process.env.JWT_SECRET_KEY;
-    const data = {
-      userEmail: email,
-      userRole: 2,
-      department: null,
-    };
-    const authToken = jwt.sign(data, jwtSecretKey);
-    return res.send({ result: 1, token: authToken });
-  }
+      const jwtSecretKey = process.env.JWT_SECRET_KEY;
+      const data = {
+        userEmail: email,
+        userRole: 2,
+        department: null,
+      };
+      const authToken = jwt.sign(data, jwtSecretKey);
+      return res.send({ result: 1, token: authToken });
+    }
 
-  if(admin_row){
-    await bcrypt.hash(password, saltRounds, async function (err, hash) {
-      await pool.query("update admins set passwd=$1 where email_id=$2", [
-        hash,email
-     ]);
-    });
-   userData = {
-    userEmail: email,
-    userRole: admin_row.admin_type,
-    department: admin_row.department,
-  };
-  
+    if (admin_row) {
+      await bcrypt.hash(password, saltRounds, async function (err, hash) {
+        await pool.query("update admins set passwd=$1 where email_id=$2", [
+          hash, email
+        ]);
+      });
+      userData = {
+        userEmail: email,
+        userRole: admin_row.admin_type,
+        department: admin_row.department,
+      };
+
       const jwtSecretKey = process.env.JWT_SECRET_KEY;
       const authToken = jwt.sign(userData, jwtSecretKey);
-    
-      switch (admin_row.admin_type) {   
-        case 0:       
+
+      switch (admin_row.admin_type) {
+        case 0:
           return res.send({ result: 4, token: authToken, admin_type: 0 });
         case 1:
           return res.send({ result: 5, token: authToken, admin_type: 1 });
         case 3:
           return res.send({ result: 6, token: authToken, admin_type: 3 });
         default:
-         
+
           return res.send({ result: 0 });
       }
-
+    }
   }
-
-
-  } 
   else {
     return res.send({ result: 0 });
   }
 
 };
-
-
-
 
 const contact_us = async (req, res) => {
   const info = req.body;
@@ -502,10 +321,7 @@ function application_submission(email, app_id, dep, spec) {
 }
 
 module.exports = {
-  signin_otp,
   signin_verify,
-  signup_otp,
-  signup_verify,
   forgot_password_otp,
   forgot_password_verify,
   contact_us,
