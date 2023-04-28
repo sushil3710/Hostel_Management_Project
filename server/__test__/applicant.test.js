4876caf83683cd31f3df08e4385b85882968d2b7
Rohit Kinha <2020csb1118@iitrpr.ac.in>
2023-04-28 16:18:25 +0530

    Student almost complete testing

--- server/__test__/applicant.test.js
+++ server/__test__/applicant.test.js
@@ -21,7 +21,7 @@ describe('GET /get-user-info', () => {
             });
         authToken = res.body.token;
         authToken1 = res1.body.token;
-    
+
     });
     it('Takes a authtoken and give the user info', async () => {
         const res = await request(app)
@@ -242,3 +242,74 @@ describe('POST /save-communication-details', () => {
     });
 
 });
+// describe('POST /save-personal-info', () => {
+//     let authToken;
+//     let authToken1;
+//     beforeAll(async () => {
+//         // get an auth token for testing purposes
+//         const res = await request(app)
+//             .post('/auth/signin/verify')
+//             .send({
+//                 email: 'rohitkinha1612@gmail.com',
+//                 password: 'root',
+//             });
+//         const res1 = await request(app)
+//             .post('/auth/signin/verify')
+//             .send({
+//                 email: '2020csb1118@iitrpr.ac.in',
+//                 password: 'root',
+//             });
+//         authToken = res.body.token;
+//         authToken1 = res1.body.token;
+
+//     });
+
+//     test('saves personal info to database with valid auth token and file', async () => {
+//         const formData = {
+//             full_name: 'John Doe',
+//             guardian: 'Jane Doe',
+//             fathers_name: 'Joe Doe',
+//             date_of_birth: '1990-01-01',
+//             aadhar_card_number: '123456789012',
+//             category: 'General',
+//             is_pwd: 'No',
+//             blood_group: 'O+',
+//             nationality: 'Indian',
+//             gender: 'Male'
+//         };
+
+//         const res = await request(app)
+//             .post('/save-personal-info')
+//             .set('Content-Type', 'multipart/form-data')
+//             .set('authorization', `${authToken}`)
+//             .field('full_name', formData.full_name)
+//             .field('guardian', formData.guardian)
+//             .field('fathers_name', formData.fathers_name)
+//             .field('date_of_birth', formData.date_of_birth)
+//             .field('aadhar_card_number', formData.aadhar_card_number)
+//             .field('category', formData.category)
+//             .field('is_pwd', formData.is_pwd)
+//             .field('blood_group', formData.blood_group)
+//             .field('nationality', formData.nationality)
+//             .field('gender', formData.gender)
+//             .attach('profile_image', './testData/profile_photo.jpg'); // replace with path to your test file
+
+//         expect(res.statusCode).toBe(200);
+//         expect(res.text).toBe('Ok');
+
+//         // check if data was saved to database
+//         const result = await pool.query(`SELECT * FROM student_info WHERE email_id = $1`, ['rohitkinha1612@gmail.com']);
+//         expect(result.rows[0].full_name).toBe(formData.full_name);
+//         expect(result.rows[0].guardian).toBe(formData.guardian);
+//         expect(result.rows[0].fathers_name).toBe(formData.fathers_name);
+//         expect(result.rows[0].date_of_birth).toBe(formData.date_of_birth);
+//         expect(result.rows[0].aadhar_card_number).toBe(formData.aadhar_card_number);
+//         expect(result.rows[0].category).toBe(formData.category);
+//         expect(result.rows[0].is_pwd).toBe(formData.is_pwd);
+//         expect(result.rows[0].blood_group).toBe(formData.blood_group);
+//         expect(result.rows[0].nationality).toBe(formData.nationality);
+//         expect(result.rows[0].gender).toBe(formData.gender);
+
+       
+//     });
+// });
